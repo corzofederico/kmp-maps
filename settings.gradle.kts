8846998e64589cb5c5590654ea25bed2f7f84614
corzofederico <corzofederico@gmail.com>
2024-12-18 16:51:48 +0100

    Edit settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
 }
 
